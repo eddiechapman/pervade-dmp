9393710d0c84c05b49f98e855da5c8c996628c4c
eddiechapman <eddie.chapman4@gmail.com>
2019-01-07 16:58:56 -0600

    Update form fields for DMP needs.

--- app/coding/forms.py
+++ app/coding/forms.py
@@ -4,10 +4,8 @@ from wtforms import BooleanField, SubmitField, TextAreaField
 class CodingForm(FlaskForm):
     """The form for recording coding information about an award."""
     pervasive_data = BooleanField('Pervasive Data')
-    data_science = BooleanField('Data Science')
-    big_data = BooleanField('Big Data')
+    review = BooleanField('Review')
     comments = TextAreaField('Comments')
-    data_synonyms = TextAreaField('Data synonyms', description='Please separate values with a semicolon')
     submit = SubmitField('Submit')
 
     def validate(self):
