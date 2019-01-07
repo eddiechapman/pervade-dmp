ee84c2af49b770f2d03cece30d2553de79edb8d4
eddiechapman <eddie.chapman4@gmail.com>
2019-01-07 16:59:34 -0600

    Add PEP8 linebreaks.

--- app/coding/forms.py
+++ app/coding/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, SubmitField, TextAreaField
 
+
 class CodingForm(FlaskForm):
     """The form for recording coding information about an award."""
     pervasive_data = BooleanField('Pervasive Data')
