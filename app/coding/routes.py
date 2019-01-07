c5dc76f786e9541d5cca64c1177b6a6d78360521
eddiechapman <eddie.chapman4@gmail.com>
2019-01-07 17:02:23 -0600

    Update coding form processing to reflect DMP fields.

--- app/coding/routes.py
+++ app/coding/routes.py
@@ -1,12 +1,10 @@
-from datetime import datetime
-from flask import render_template, flash, redirect, url_for, session, current_app
+from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_required
+
 from app import db
+from app.coding import bp
 from app.coding.forms import CodingForm
 from app.models import Award, Code
-from app.coding import bp
-
-
 
 
 @bp.route('/')
@@ -25,7 +23,6 @@ def get_award():
     Returns:
          Redirect to 404 error page if no awards are found.
     """
-
     awards = Award.query.all()
     for award in awards:
         if not award.codes:
@@ -36,10 +33,10 @@ def get_award():
 @login_required
 def code_award(award_id):
     """Display award data and coding form. Process form submission.
-    
+
     Args:
         award_id (int): The ID of the current award.
-        
+
     Returns:
         Redirect to coding form on first visit, or a redirect to
             get_award on form submit.
@@ -51,9 +48,7 @@ def code_award(award_id):
     if form.validate_on_submit():
         code = Code(
             pervasive_data=form.pervasive_data.data,
-            data_science=form.data_science.data,
-            big_data=form.big_data.data,
-            data_synonyms=form.data_synonyms.data,
+            review=form.review.data,
             comments=form.comments.data
         )
         code.award = award
